96b83807c05ed741168429aaeab27f93ce810f04
jbveara <67393204+jbveara@users.noreply.github.com>
2023-09-13 13:48:10 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+//Name:
+//Instructor:
+
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+//declare your variables
+
+
+//Get user input
+
+
+//Write your while loop
+  while()
+    {
+
+
+
+
+    }
+//Write your switch case
+  switch(){
+
+
+
+
+
+
+
+  }
+
+return 0;
+}
